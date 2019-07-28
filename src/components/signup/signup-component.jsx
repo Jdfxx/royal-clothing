f7faabf9e2693e920eb93e55e9be7d30860af968
fhagno@yahoo.com <fhagno@yahoo.com>
2019-07-28 13:11:09 +0200

    Adding signup mock component

---
+++ src/components/signup/signup-component.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import FormInput from "../FormInput/form-input.component";
+import './signup.styles.scss';
+import CustomButton from "../CustomButton/custom-button.component";
+
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      email: "",
+      password: ""
+    });
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <div className="sign-up">
+        <h2>I dont have an account yet</h2>
+        <span className="subtitle">Sign up</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="email"
+            id="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="email"
+          />
+          <FormInput
+            type="password"
+            name="password"
+            id="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="password"
+          />
+          <CustomButton type='submit'>Submit</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Signup;
--- src/pages/sign-and-sign-up/sign-and-sign-up.component.jsx
+++ src/pages/sign-and-sign-up/sign-and-sign-up.component.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import './sign-and-sign-up.styles.scss'
-import Signin from '../../components/signin/signin.component';
+import React from "react";
+import "./sign-and-sign-up.styles.scss";
+import Signin from "../../components/signin/signin.component";
+import Signup from "../../components/signup/signup-component";
 
-
-const signInAndSignup = ()=> {
-   
-        return (
-            <div className="sign-in-and-sign-up">
-               <Signin />
-            </div>
-        );
-   
-}
+const signInAndSignup = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <Signin />
+      <Signup />
+    </div>
+  );
+};
 
 export default signInAndSignup;

ebaca4bf8e327ca5584ace1c39e425f39b1fdb29
fhagno@yahoo.com <fhagno@yahoo.com>
2019-08-14 11:39:05 +0200

    Added cart quantity component

--- src/components/CartIcon/CartIcon.component.jsx
+++ src/components/CartIcon/CartIcon.component.jsx
@@ -4,13 +4,20 @@ import './CartIcon.styles.scss';
 import {connect} from 'react-redux';
 import {toggleCartHidden} from "../../store/cart/cart.actions";
 
-const CartIcon = ({toggleCartHidden}) => {
+const CartIcon = ({toggleCartHidden, itemsCount}) => {
+
     return (
         <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
+            <span className="item-count">{itemsCount}</span>
         </div>
     );
 };
 
-export default connect(null, {toggleCartHidden})(CartIcon);
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    itemsCount: cartItems.reduce((acc, item) => {
+        return acc + item.quantity;
+    }, 0)
+});
+
+export default connect(mapStateToProps, {toggleCartHidden})(CartIcon);

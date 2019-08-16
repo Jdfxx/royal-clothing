76450e810e1d77a1f9c3216e4116c94bdffe038c
fhagno@yahoo.com <fhagno@yahoo.com>
2019-08-16 13:48:29 +0200

    Fixing route bug

--- src/store/directory/directory.reducer.js
+++ src/store/directory/directory.reducer.js
@@ -23,14 +23,14 @@ const initialState = {
       imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
       size: "large",
       id: 4,
-      linkUrl: "shop/women"
+      linkUrl: "shop/womens"
     },
     {
       title: "mens",
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
       size: "large",
       id: 5,
-      linkUrl: "shop/men"
+      linkUrl: "shop/mens"
     }
   ]
 };

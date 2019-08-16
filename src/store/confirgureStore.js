c99248ade7984cae8b6249cea9878bc455c29ecc
fhagno@yahoo.com <fhagno@yahoo.com>
2019-08-16 14:14:34 +0200

    Moving logger to dev mode only

--- src/store/confirgureStore.js
+++ src/store/confirgureStore.js
@@ -2,9 +2,14 @@ import {applyMiddleware, createStore} from "redux";
 import rootReducer from "./root.reducer";
 import {composeWithDevTools} from "redux-devtools-extension";
 import * as actionCreators from './index';
+import logger from 'redux-logger';
 import {persistStore} from "redux-persist";
 
 const middleware = [];
+
+if(process.env.NODE_ENV === 'development') {
+    middleware.push(logger);
+};
 
 const composeEnhancers = composeWithDevTools({
     actionCreators,
